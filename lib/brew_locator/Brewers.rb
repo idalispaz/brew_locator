fd3166527872c774498c12e4c31730da7898361a
idalispaz <idalispaz@gmail.com>
2020-08-09 15:19:03 -0700

    first commit

--- lib/brew_locator/Brewers.rb
+++ lib/brew_locator/Brewers.rb
@@ -1,10 +1,13 @@
 class Brewers
-    attr_reader :name, :street
+    attr_reader :name, :street, :brewery_type, :city, :website_url
     @@all = []
 
     def initialize(brewery_info)
         @name = brewery_info["name"] 
         @street = brewery_info ["street"]
+        @city = brewery_info ["city"]
+        @brewery_type = brewery_info["brewery_type"]
+        @website_url = brewery_info ["website_url"]
         @@all << self 
     end 
 
@@ -17,7 +20,15 @@ class Brewers
     end 
 
     def print_pretty
-        puts @name 
+        puts @name.colorize(:yellow)
+        puts "\n"
         puts @street
+        puts "\n"
+        puts @city 
+        puts "\n"
+        puts @brewery_type
+        puts "\n"
+        puts @website_url
+        puts "\n"
     end 
 end 
